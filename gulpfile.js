0071a91226838eec0ef09048927a034fa396c346
Maurizio Casimirri <maurizio.casimirri@auctionata.com>
2015-10-14 15:13:34 +0200

    added gulp release task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp      = require( 'gulp' );
 var jasmine   = require( 'gulp-jasmine');
 var jshint    = require( 'gulp-jshint');
-var shell     = require( 'gulp-shell');
+
+require('gulp-release-tasks')(gulp);
 
 gulp.task('test', function () {
     return gulp.src(['test/**/*.spec.js'])
